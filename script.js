081f7784b4f627db4771c4c775682f2187283934
flynndcs <21161368+flynndcs@users.noreply.github.com>
2018-03-24 14:45:04 -0500

    update js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert('connected');
+
+$('#about-button').click(function() {
+	$('html, body').animate({
+		scrollTop: $('#about-section').offset().top
+	}, 2000);
+});
+
+$('#skills-button').click(function() {
+	$('html, body').animate({
+		scrollTop: $('#skills-section').offset().top
+	}, 2000);
+});
+$('#contact-button').click(function() {
+	$('html, body').animate({
+		scrollTop: $('#contact-section').offset().top
+	}, 2000);
+});
+
+// $("#about-button").click(function() {
+// 	console.log("clicked");
+// });
